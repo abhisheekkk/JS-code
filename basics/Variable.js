0dbd118b78d7c24865dc2eaa6f4a7c89f3a01a50
Abhishek kumar <98544462+abhisheekkk@users.noreply.github.com>
2024-01-31 14:45:37 +0000

    container confg done

---
+++ basics/Variable.js
@@ -0,0 +1,27 @@
+// Topic:- VARIABLE
+
+
+// console.log("Abhishek")
+
+// console.log(profile);
+// console.log(typeof profile["isFollow"]);
+const accountId= 145553
+let accountEmail = "abhishekk2086.com"
+var accountPassword = "12356"
+accountCity = "Banglore"
+let accountState;
+
+//accountId = 2 // is not allowed
+
+accountEmail ="hclhp@mail.com"
+accountPassword = "212525"
+accountCity = "Texas"
+
+console.log(accountId);
+
+/*
+ prefer not to use var
+ because of issue in block scope and functional scope.
+*/
+
+console.table([accountId,accountEmail,accountPassword, accountCity, accountState])
