4a9f0204e7ea2e1fd71d835a4649dc2a7caf3ce5
Abhishek kumar <98544462+abhisheekkk@users.noreply.github.com>
2024-02-01 14:02:55 +0000

    conversion of datatypes

---
+++ basics/ conversionopr.js
@@ -0,0 +1,37 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true 
+// let score = "Abhishek"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/*
+"33" => 33
+"33abc" => NaN
+true =>1; false=>0
+*/
+
+// let isloggedIn = 1
+
+// let booleanIsLoggedIn = Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+1 => true ; 0 =>false
+" " => false 
+"Abhishek" => true
+*/
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
+/*
+It is depends on String(), what we are typing like Boolean, String then it shows according datatypes
+*/
