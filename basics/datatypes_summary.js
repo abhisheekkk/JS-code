8310dc74614c427c9999511d6a11e443cc6874f2
Abhishek kumar <98544462+abhisheekkk@users.noreply.github.com>
2024-03-08 16:38:28 +0000

    datatypes

---
+++ basics/datatypes_summary.js
@@ -0,0 +1,33 @@
+// primitive
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbool, Bight
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id= symbol('123')
+const anotherId = symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456545376548963n
+
+// reference(Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Shaktiman", "Naagraj", "doga"];
+let myObj = {
+    name : "hitesh", 
+    age : 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
